a9952e149389f6382bbcaeea153360859accbe12
Edson Saraiva de Almeida <68782201+ffvprogweb@users.noreply.github.com>
2021-10-09 11:41:26 -0300

    Cap11

---
+++ cap11/src/main/java/com/fatec/cap11/Cap11Application.java
@@ -0,0 +1,13 @@
+package com.fatec.cap11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cap11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cap11Application.class, args);
+	}
+
+}
